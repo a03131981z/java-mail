5809ef6141483b64e1fb68b8dfc3fcb3794a8835
Lindembergh Madruga <lindemberghferreira@gmail.com>
2021-10-08 07:42:28 -0300

    Criando a classe ObjetoEnviaEmail para reutilizar

---
+++ src/main/java/enviando/mail/limadruga/ObjetoEnviaEmail.java
@@ -0,0 +1,54 @@
+package enviando.mail.limadruga;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class ObjetoEnviaEmail {
+
+	private String userName = "mlindembergh@gmail.com";
+	private String senha = "Arthur2011";
+	private String listaDestinatarios = "";
+	private String nomeRemetente = "";
+	private String assuntoEmail = "";
+	private String textoEmail = "";
+	
+	public void enviaEmail() throws MessagingException {
+	
+		Properties properties = new Properties();
+		properties.put("mail.smtp.ssl.trust", "*");
+		properties.put("mail.smtp.auth", "true");/*Autorização*/
+		properties.put("mail.smtp.starttls", "true");/*Autenticação*/
+		properties.put("mail.smtp.host","smtp.gmail.com");/*Servidor gmail Google*/
+		properties.put("mail.smtp.port", "465");/*Porta do servidor*/
+		properties.put("mail.smtp.socketFactory.port", "465");/*Especifica a porta a ser conectada pelo socket*/
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");/*Classe socket de conexão ao SMTP*/
+		
+		Session session = Session.getInstance(properties, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, senha);
+			}
+		});
+		Address[] toUser = InternetAddress.parse(listaDestinatarios);
+		Message message = new MimeMessage(session);
+		try {
+			message.setFrom(new InternetAddress(userName, nomeRemetente));/*Quem está enviando*
+		} catch (UnsupportedEncodingException | MessagingException e) {
+			e.printStackTrace();
+		}
+		
+		message.setRecipients(Message.RecipientType.TO, toUser);/*Email de destino*/
+		message.setSubject(assuntoEmail); /*Assunto do email*/
+		message.setText(textoEmail);
+		
+		Transport.send(message);
+	}
+}
--- src/test/java/enviando/mail/limadruga/AppTest.java
+++ src/test/java/enviando/mail/limadruga/AppTest.java
@@ -1,53 +1,16 @@
 package enviando.mail.limadruga;
-import java.util.Properties;
-import javax.mail.Address;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.mail.MessagingException;
 
 public class AppTest {
-	
-	private String userName = "mlindembergh@gmail.com";
-	private String senha = "Arthur2011";
     
     @org.junit.Test
-    public void testeEmail() {
+    public void testeEmail() throws InterruptedException, MessagingException {
+    	
+    	ObjetoEnviaEmail enviaEmail = new ObjetoEnviaEmail("mlindembergh@gmail.com", "Alex do JDev Treinamentos", "Testando e-mail com Java", "Esse texto é a descrição do meu e-mail");
+    	
+    	enviaEmail.enviaEmail();
     	/*Olhe as configurações smtp do seu email*/
-		try {
-			Properties properties = new Properties();
-			
-			properties.put("mail.smtp.ssl.trust", "*");
-			properties.put("mail.smtp.auth", "true");/*Autorização*/
-			properties.put("mail.smtp.starttls", "true");/*Autenticação*/
-			properties.put("mail.smtp.host","smtp.gmail.com");/*Servidor gmail Google*/
-			properties.put("mail.smtp.port", "465");/*Porta do servidor*/
-			properties.put("mail.smtp.socketFactory.port", "465");/*Especifica a porta a ser conectada pelo socket*/
-			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");/*Classe socket de conexão ao SMTP*/
-			
-			Session session = Session.getInstance(properties, new Authenticator() {
-				@Override
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(userName, senha);
-				}
-			});
-			Address[] toUser = InternetAddress.parse("mlindembergh@gmail.com, lindemberghferreira@gmail.com, arthurraquel3@gmail.com");
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(userName, "Alex JDev - Treinamentos"));/*Quem está enviando*/
-			message.setRecipients(Message.RecipientType.TO, toUser);/*Email de destino*/
-			message.setSubject("Chegou email enviado com Java"); /*Assunto do email*/
-			message.setText("Olá programador, vc acaba de receber um email enviado com Java do curso Formação Java Web do Alex");
-			
-			Transport.send(message);
-			
-			/*Caso o email não esteja sendo enviado então coloque um tempo de espera mais isso só pode ser usado para testes.*/
-			Thread.sleep(5000);
-			
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
+		/*Caso o email não esteja sendo enviado então coloque um tempo de espera mais isso só pode ser usado para testes.*/
+		Thread.sleep(5000);
     }
 }
